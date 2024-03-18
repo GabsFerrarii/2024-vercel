a1825dd7ff8a587d20e107d618b69c940d724158
Gabriel Ferreira <84422577+GabsFerrarii@users.noreply.github.com>
2024-03-18 15:52:17 -0300

    Update App.tsx
    Aprendizado de classes e objetos

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,71 @@
+import "./App.css";
+import { useReducer } from "react";
+
+const MeuBotao = (props:any) => {
+  return <button>{props.titulo}</button>;
+}
+
+const Usuario = (props:any) => {
+  return <h1>{props.nome}</h1>;
+}
+
+/* const ListaDeTarefas = (props:any) => {
+  return (
+    <>
+      <h4>Lista de Tarefas</h4>
+      <ul>
+        {
+         props.dados.map((item) => 
+          return <ItemTarefa key={item.id} titulo={item.titulo} />
+        }
+      </ul>
+    </>
+  )
+}
+
+const ItemTarefa = (props:any) => {
+  return (<li>{titulo}</li>)
+}*/
+
 const App = () => {
+  let usuario = {nome: "Gabras"};
+  const tarefas = [
+    "Tarefa 1",
+    "Tarefa 2"
+  ];
+  const tarefasNoMundoReal = [
+    {
+      id: 1,
+      titulo: "Criar Coisas",
+      concluido: false
+    },
+    {
+      id: 2,
+      titulo: "Criar outras Coisas",
+      concluido: true
+    }
+  ]
   return (
     <div>
       <h1>Bem vindo ao mundo React</h1>
-      <button>eu sou um botão html</button>
+      {
+        usuario ?
+          (<Usuario nome={usuario.nome} />) :
+          (<MeuBotao titulo="Login" />)
+      }
+      {/*{
+        !usuario && (<MeuBotao titulo="Login" />)
+      }*/}
+      <MeuBotao titulo="botao legal" />
+      <MeuBotao titulo="botao maneiro" />
+      <MeuBotao titulo="botao interessante" />
+      <h4>Tarefas lista de strings</h4>
+      <ul>
+        {
+          tarefas.map((item, indice) => {return (<li key={indice}>{item}</li>)})
+        }
+      </ul>
+      {/* <ListaDeTarefas dados={tarefasNoMundoReal} /> */}
     </div>
   );
 }
